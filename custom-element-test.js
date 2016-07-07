5649251da9b31b5f89b57892b7fff2283f2adce8
DimitarD <dimitar@jqwidgets.com>
2016-07-07 14:50:53 +0300

    Added a new "it" test for shadow DOM

--- custom-element-test.js
+++ custom-element-test.js
@@ -2,4 +2,14 @@
     it('registerElement is defined', function () {
         expect(document.registerElement).toBeDefined();
     });
+
+    it('children are hidden (in Shadow DOM)', function () {
+        // a jqx-hoy element is dynamically appended
+        var dynamicElement = document.createElement('div');
+        dynamicElement.innerHTML = '<jqx-hoy id="jqxHoy" imgSrc="https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg" imgLabel="Sun2"></jqx-hoy>';
+        document.body.appendChild(dynamicElement);
+        var jqxHoy = document.getElementById('jqxHoy');
+
+        expect(jqxHoy.children.length).toBe(0);
+    });
 });
